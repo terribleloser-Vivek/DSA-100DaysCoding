6d91e7821c5294ea660d641b9d287305e010a750
VIVEK KUMAR SINGH <86074003+terribleloser-Vivek@users.noreply.github.com>
2022-01-20 23:05:50 +0530

    https://practice.geeksforgeeks.org/problems/sum-of-array-elements2502/1/?category[]=Arrays&category[]=Arrays&problemStatus=unsolved&page=1&query=category[]ArraysproblemStatusunsolvedpage1category[]Arrays

---
+++ array/sum of elemnts.c
@@ -0,0 +1,13 @@
+//Complete this function
+
+int sumElement(int arr[],int n)
+{
+     int sum=0;
+    for( int i=0;i<n;i++)
+    {
+       
+        sum=sum+arr[i]; 
+    }
+    
+    return sum;
+}
